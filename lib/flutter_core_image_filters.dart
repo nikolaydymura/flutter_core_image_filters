82c0a624177b39278691834132f818a8b188fc2f
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-18 16:41:06 +0200

    Modified structure

--- lib/flutter_core_image_filters.dart
+++ lib/flutter_core_image_filters.dart
@@ -1,7 +1,7 @@
 library flutter_core_image_filters;
 
 import 'dart:async';
-import 'dart:io' show File, Platform;
+import 'dart:io' show File;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -39,7 +39,7 @@ part 'src/configurations/ci_linear_dodge_blend_mode.dart';
 part 'src/configurations/ci_luminosity_blend_mode.dart';
 part 'src/configurations/ci_maximum_compositing.dart';
 part 'src/configurations/ci_minimum_compositing.dart';
-part 'src/configurations/ci_monochrome.dart';
+part 'src/configurations/ci_color_monochrome.dart';
 part 'src/configurations/ci_multiply_blend_mode.dart';
 part 'src/configurations/ci_multiply_compositing.dart';
 part 'src/configurations/ci_no_parameters_configurations.dart';
@@ -63,72 +63,75 @@ part 'src/parameters/ns_data_parameter.dart';
 part 'src/parameters/ns_number_parameter.dart';
 part 'src/parameters/slider_ns_number_parameter.dart';
 
-Map<String, CIFilterConfiguration Function()> availableFilters = Platform.isIOS
-    ? {
-        'Addition Compositing': () => CIAdditionCompositingConfiguration(),
-        'Barcode Generator': () => CIBarcodeGeneratorConfiguration(),
-        'Blend With Alpha Mask': () => CIBlendWithAlphaMaskConfiguration(),
-        'Blend With Blue Mask': () => CIBlendWithBlueMaskConfiguration(),
-        'Blend With Mask': () => CIBlendWithMaskConfiguration(),
-        'Blend With Red Mask': () => CIBlendWithRedMaskConfiguration(),
-        'Random Generator': () => CIRandomGeneratorConfiguration(),
-        'Color Absolute Difference': () =>
-            CIColorAbsoluteDifferenceConfiguration(),
-        'Color Blend Mode': () => CIColorBlendModeConfiguration(),
-        'Color Burn Blend Mode': () => CIColorBurnBlendModeConfiguration(),
-        'Color Cube': () => CIColorCubeConfiguration(),
-        'Color Dodge Blend Mode': () => CIColorDodgeBlendModeConfiguration(),
-        'Color Invert': () => CIColorInvertConfiguration(),
-        'Color Map': () => CIColorMapConfiguration(),
-        'Color Threshold Otsu': () => CIColorThresholdOtsuConfiguration(),
-        'Comic Effect': () => CIComicEffectConfiguration(),
-        'Darken Blend Mode': () => CIDarkenBlendModeConfiguration(),
-        'Depth To Disparity': () => CIDepthToDisparityConfiguration(),
-        'Difference Blend Mode': () => CIDifferenceBlendModeConfiguration(),
-        'Disparity To Depth': () => CIDisparityToDepthConfiguration(),
-        'Divide Blend Mode': () => CIDivideBlendModeConfiguration(),
-        'Exclusion Blend Mode': () => CIExclusionBlendModeConfiguration(),
-        'Gabor Gradients': () => CIGaborGradientsConfiguration(),
-        'Hard Light Blend Mode': () => CIHardLightBlendModeConfiguration(),
-        'Hue Blend Mode': () => CIHueBlendModeConfiguration(),
-        'Lighten Blend Mode': () => CILightenBlendModeConfiguration(),
-        'Linear Burn Blend Mode': () => CILinearBurnBlendModeConfiguration(),
-        'Linear Dodge Blend Mode': () => CILinearDodgeBlendModeConfiguration(),
-        'Linear To SRGB Tone Curve': () =>
-            CILinearToSRGBToneCurveConfiguration(),
-        'Luminosity Blend Mode': () => CILuminosityBlendModeConfiguration(),
-        'Mask ToAlpha': () => CIMaskToAlphaConfiguration(),
-        'Maximum Component': () => CIMaximumComponentConfiguration(),
-        'Maximum': () => CIMaximumCompositingConfiguration(),
-        'Median Filter': () => CIMedianFilterConfiguration(),
-        'Minimum Component': () => CIMinimumComponentConfiguration(),
-        'Minimum': () => CIMinimumCompositingConfiguration(),
-        'Monochrome': () => CIMonochromeConfiguration(),
-        'Multiply Blend Mode': () => CIMultiplyBlendModeConfiguration(),
-        'Multiply': () => CIMultiplyCompositingConfiguration(),
-        'Overlay Blend Mode': () => CIOverlayBlendModeConfiguration(),
-        'Photo Effect Chrome': () => CIPhotoEffectChromeConfiguration(),
-        'PhotoEffectFade': () => CIPhotoEffectFadeConfiguration(),
-        'PhotoEffectInstant': () => CIPhotoEffectInstantConfiguration(),
-        'PhotoEffectMono': () => CIPhotoEffectMonoConfiguration(),
-        'PhotoEffectNoir': () => CIPhotoEffectNoirConfiguration(),
-        'PhotoEffectProcess': () => CIPhotoEffectProcessConfiguration(),
-        'PhotoEffectTonal': () => CIPhotoEffectTonalConfiguration(),
-        'PhotoEffectTransfer': () => CIPhotoEffectTransferConfiguration(),
-        'Pin Light Blend Mode': () => CIPinLightBlendModeConfiguration(),
-        'SRGB Tone Curve To Linear': () =>
-            CISRGBToneCurveToLinearConfiguration(),
-        'Saliency Map Filter': () => CISaliencyMapFilterConfiguration(),
-        'Sample Nearest': () => CISampleNearestConfiguration(),
-        'Saturation Blend Mode': () => CISaturationBlendModeConfiguration(),
-        'Screen Blend Mode': () => CIScreenBlendModeConfiguration(),
-        'Soft Light Blend Mode': () => CISoftLightBlendModeConfiguration(),
-        'Source Atop': () => CISourceAtopCompositingConfiguration(),
-        'Source In': () => CISourceInCompositingConfiguration(),
-        'Source Out': () => CISourceOutCompositingConfiguration(),
-        'Source Over': () => CISourceOverCompositingConfiguration(),
-        'Subtract Blend Mode': () => CISubtractBlendModeConfiguration(),
-        'Thermal': () => CIThermalConfiguration(),
-        'XRay': () => CIXRayConfiguration(),
-      }
-    : {};
+class FlutterCoreImageFilters {
+  static Iterable<String> get availableFilters => _availableFilters.keys;
+  static final Map<String, CIFilterConfiguration Function()> _availableFilters =
+      {
+    'Addition Compositing': () => CIAdditionCompositingConfiguration(),
+    'Barcode Generator': () => CIBarcodeGeneratorConfiguration(),
+    'Blend With Alpha Mask': () => CIBlendWithAlphaMaskConfiguration(),
+    'Blend With Blue Mask': () => CIBlendWithBlueMaskConfiguration(),
+    'Blend With Mask': () => CIBlendWithMaskConfiguration(),
+    'Blend With Red Mask': () => CIBlendWithRedMaskConfiguration(),
+    'Random Generator': () => CIRandomGeneratorConfiguration(),
+    'Color Absolute Difference': () => CIColorAbsoluteDifferenceConfiguration(),
+    'Color Blend Mode': () => CIColorBlendModeConfiguration(),
+    'Color Burn Blend Mode': () => CIColorBurnBlendModeConfiguration(),
+    'Color Cube': () => CIColorCubeConfiguration(),
+    'Color Dodge Blend Mode': () => CIColorDodgeBlendModeConfiguration(),
+    'Color Invert': () => CIColorInvertConfiguration(),
+    'Color Map': () => CIColorMapConfiguration(),
+    'Color Threshold Otsu': () => CIColorThresholdOtsuConfiguration(),
+    'Comic Effect': () => CIComicEffectConfiguration(),
+    'Darken Blend Mode': () => CIDarkenBlendModeConfiguration(),
+    'Depth To Disparity': () => CIDepthToDisparityConfiguration(),
+    'Difference Blend Mode': () => CIDifferenceBlendModeConfiguration(),
+    'Disparity To Depth': () => CIDisparityToDepthConfiguration(),
+    'Divide Blend Mode': () => CIDivideBlendModeConfiguration(),
+    'Exclusion Blend Mode': () => CIExclusionBlendModeConfiguration(),
+    'Gabor Gradients': () => CIGaborGradientsConfiguration(),
+    'Hard Light Blend Mode': () => CIHardLightBlendModeConfiguration(),
+    'Hue Blend Mode': () => CIHueBlendModeConfiguration(),
+    'Lighten Blend Mode': () => CILightenBlendModeConfiguration(),
+    'Linear Burn Blend Mode': () => CILinearBurnBlendModeConfiguration(),
+    'Linear Dodge Blend Mode': () => CILinearDodgeBlendModeConfiguration(),
+    'Linear To SRGB Tone Curve': () => CILinearToSRGBToneCurveConfiguration(),
+    'Luminosity Blend Mode': () => CILuminosityBlendModeConfiguration(),
+    'Mask ToAlpha': () => CIMaskToAlphaConfiguration(),
+    'Maximum Component': () => CIMaximumComponentConfiguration(),
+    'Maximum': () => CIMaximumCompositingConfiguration(),
+    'Median Filter': () => CIMedianFilterConfiguration(),
+    'Minimum Component': () => CIMinimumComponentConfiguration(),
+    'Minimum': () => CIMinimumCompositingConfiguration(),
+    'Monochrome': () => CIMonochromeConfiguration(),
+    'Multiply Blend Mode': () => CIMultiplyBlendModeConfiguration(),
+    'Multiply': () => CIMultiplyCompositingConfiguration(),
+    'Overlay Blend Mode': () => CIOverlayBlendModeConfiguration(),
+    'Photo Effect Chrome': () => CIPhotoEffectChromeConfiguration(),
+    'Photo Effect Fade': () => CIPhotoEffectFadeConfiguration(),
+    'Photo Effect Instant': () => CIPhotoEffectInstantConfiguration(),
+    'Photo Effect Mono': () => CIPhotoEffectMonoConfiguration(),
+    'Photo Effect Noir': () => CIPhotoEffectNoirConfiguration(),
+    'Photo Effect Process': () => CIPhotoEffectProcessConfiguration(),
+    'Photo Effect Tonal': () => CIPhotoEffectTonalConfiguration(),
+    'Photo Effect Transfer': () => CIPhotoEffectTransferConfiguration(),
+    'Pin Light Blend Mode': () => CIPinLightBlendModeConfiguration(),
+    'SRGB Tone Curve To Linear': () => CISRGBToneCurveToLinearConfiguration(),
+    'Saliency Map Filter': () => CISaliencyMapFilterConfiguration(),
+    'Sample Nearest': () => CISampleNearestConfiguration(),
+    'Saturation Blend Mode': () => CISaturationBlendModeConfiguration(),
+    'Screen Blend Mode': () => CIScreenBlendModeConfiguration(),
+    'Soft Light Blend Mode': () => CISoftLightBlendModeConfiguration(),
+    'Source Atop': () => CISourceAtopCompositingConfiguration(),
+    'Source In': () => CISourceInCompositingConfiguration(),
+    'Source Out': () => CISourceOutCompositingConfiguration(),
+    'Source Over': () => CISourceOverCompositingConfiguration(),
+    'Subtract Blend Mode': () => CISubtractBlendModeConfiguration(),
+    'Thermal': () => CIThermalConfiguration(),
+    'XRay': () => CIXRayConfiguration(),
+  };
+
+  static CIFilterConfiguration? createFilter({required String displayName}) {
+    return _availableFilters[displayName]?.call();
+  }
+}
