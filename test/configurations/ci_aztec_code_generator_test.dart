43d4858d68155aac585b97596f7ff7f614768bf3
EgorEko <81403804+EgorEko@users.noreply.github.com>
2022-12-14 16:26:53 +0200

    Create tests from CIAreaMinimum-CIBlendWithAlphaMask

---
+++ test/configurations/ci_aztec_code_generator_test.dart
@@ -0,0 +1,53 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_core_image_filters/flutter_core_image_filters.dart';
+import 'package:flutter_gpu_filters_interface/flutter_gpu_filters_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late CIAztecCodeGeneratorConfiguration configuration;
+  setUp(() {
+    configuration = CIAztecCodeGeneratorConfiguration();
+  });
+  group('CIAztecCodeGenerator', () {
+    test('verify name', () {
+      expect(configuration.name, 'CIAztecCodeGenerator');
+    });
+    test('verify inputKeys', () {
+      final parameters =
+          configuration.parameters.map((e) => e.name).toSet().sorted();
+      expect(
+        parameters,
+        ['inputCompactStyle', 'inputCorrectionLevel', 'inputLayers'],
+      );
+      final names =
+          configuration.parameters.map((e) => e.displayName).toSet().sorted();
+      expect(
+        names,
+        ['CompactStyle', 'CorrectionLevel', 'Layers'],
+      );
+    });
+    test('change inputCompactStyle', () {
+      final parameter = configuration.parameters
+          .firstWhere((e) => e.name == 'inputCompactStyle') as NumberParameter;
+      expect(parameter.value, 0.0);
+      configuration.compactStyle = 0.5;
+      expect(parameter.value, 0.5);
+    });
+
+    test('change inputCorrectionLevel', () {
+      final parameter = configuration.parameters
+              .firstWhere((e) => e.name == 'inputCorrectionLevel')
+          as NumberParameter;
+      expect(parameter.value, 23);
+      configuration.correctionLevel = 50;
+      expect(parameter.value, 50);
+    });
+    test('change inputLayers', () {
+      final parameter = configuration.parameters
+          .firstWhere((e) => e.name == 'inputLayers') as NumberParameter;
+      expect(parameter.value, 1);
+      configuration.layers = 20;
+      expect(parameter.value, 20);
+    });
+  });
+}
---
+++ test/configurations/ci_barcode_generator_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_core_image_filters/flutter_core_image_filters.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late CIBarcodeGeneratorConfiguration configuration;
+  setUp(() {
+    configuration = CIBarcodeGeneratorConfiguration();
+  });
+  group('CIBarcodeGenerator', () {
+    test('verify name', () {
+      expect(configuration.name, 'CIBarcodeGenerator');
+    });
+  });
+}
