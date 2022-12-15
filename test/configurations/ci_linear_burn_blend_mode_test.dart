25040314b626146bc83bd5917fe1e6638edaecc3
EgorEko <81403804+EgorEko@users.noreply.github.com>
2022-12-15 19:35:02 +0200

    Created tests KeystoneCorrectionCombined - LinearBurnBlendMode

---
+++ test/configurations/ci_linear_burn_blend_mode_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_core_image_filters/flutter_core_image_filters.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late CILinearBurnBlendModeConfiguration configuration;
+  setUp(() {
+    configuration = CILinearBurnBlendModeConfiguration();
+  });
+  group('CILinearBurnBlendMode', () {
+    test('verify name', () {
+      expect(configuration.name, 'CILinearBurnBlendMode');
+    });
+  });
+}
