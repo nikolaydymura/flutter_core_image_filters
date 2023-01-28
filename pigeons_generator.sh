6adab0672fe42a08d39bb7299a014917db7b1f49
Nikolay Dymura <nikolay.dymura@gmail.com>
2023-01-29 00:18:12 +0200

    Modified .pubignore

--- pigeons_generator.sh
+++ pigeons_generator.sh
@@ -2,7 +2,8 @@
 flutter pub run pigeon --input pigeons/messages_preview.dart
 flutter pub run pigeon --input pigeons/messages_filter.dart
 
-cp -r ios/Classes/CILookupTableFilter.swift macos/Classes/
+cp -r ios/Classes/Filters/CILookupTableFilter.swift macos/Classes/
+cp -r ios/Classes/Filters/CISquareLookupTableFilter.swift macos/Classes/
 cp -r ios/Classes/CoreImageFilters.swift macos/Classes/
 cp -r ios/Classes/FilterMessages.g.h macos/Classes/
 cp -r ios/Classes/FilterMessages.g.m macos/Classes/
@@ -17,4 +18,4 @@ sed -i '' -e 's/import UIKit//' macos/Classes/ImagePreview.swift
 sed -i '' -e 's:#import <Flutter/Flutter.h>:#import <FlutterMacOS/FlutterMacOS.h>:' macos/Classes/PreviewMessages.g.m
 sed -i '' -e 's:#import <Flutter/Flutter.h>:#import <FlutterMacOS/FlutterMacOS.h>:' macos/Classes/FilterMessages.g.m
 
-flutter pub run build_runner build --delete-conflicting-outputs
+#flutter pub run build_runner build --delete-conflicting-outputs
